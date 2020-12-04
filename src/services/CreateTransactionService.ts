ad438aa1dc0292f2a49ef6a5f28b9e3c1815e484
BismarckOliveira <bismarckoliver@hotmail.com>
2020-12-04 13:30:50 -0300

    Configurnado migrations

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,31 @@
-// import AppError from '../errors/AppError';
-
+import { getRepository } from 'typeorm';
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category_id: Category;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category_id,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getRepository(Transaction);
+
+    const newTranscation = transactionRepository.create({
+      title,
+      value,
+      type,
+      category_id,
+    });
+
+    await transactionRepository.save(newTranscation);
   }
 }
 
